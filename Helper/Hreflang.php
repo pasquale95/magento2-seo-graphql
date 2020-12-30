0dec11ce88c963662600315231b0a3a7009b70fe
Pasquale95 <pasqualeconvertini95@gmail.com>
2020-12-30 09:11:14 +0100

    added email

--- Helper/Hreflang.php
+++ Helper/Hreflang.php
@@ -1,6 +1,7 @@
 <?php
 /**
- * @author Pasquale Convertini (@Pasquale95)
+ * @author: Pasquale Convertini <pasqualeconvertini95@gmail.com>
+ * @github: @Pasquale95
  *
  * This file is subject to the terms and conditions defined in
  * file 'LICENSE', which is part of this source code package.
