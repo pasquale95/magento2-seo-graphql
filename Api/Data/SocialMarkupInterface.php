a22a2c172405a75f41cca019ef12ee87a0ef73dc
Pasquale95 <pasqualeconvertini95@gmail.com>
2021-01-19 19:03:06 +0100

    abstracted socialMarkup

--- Api/Data/SocialMarkupInterface.php
+++ Api/Data/SocialMarkupInterface.php
@@ -16,24 +16,6 @@ namespace Paskel\Seo\Api\Data;
 interface SocialMarkupInterface
 {
     /**
-     * Constants for the Resolver array
-     */
-    const PROPERTY = "property";
-    const CONTENT = "content";
-
-    /**
-     * Constants defined for keys of array.
-     * OpenGraph based.
-     */
-    const TYPE = "og:type";
-    const LOCALE = "og:locale";
-    const SITENAME = "og:site_name";
-    const URL = "og:url";
-    const TITLE = "og:title";
-    const DESCRIPTION = "og:description";
-    const IMAGE = "og:image";
-
-    /**
      * Name for the db field where to store the
      * image url.
      */
@@ -43,45 +25,4 @@ interface SocialMarkupInterface
      * Relative path where placeholder images are stored.
      */
     const PLACEHOLDER_FOLDER = "seo/socialMarkup/placeholder";
-
-    /**
-     * Constants for those values that don't change among the graphql calls
-     */
-    const TYPE_VALUE = "website";
-    const SITENAME_VALUE = "example";
-
-    /**
-     * @param $type
-     */
-    public function setType($type);
-
-    /**
-     * @param $locale
-     */
-    public function setLocale($locale);
-
-    /**
-     * @param $sitename
-     */
-    public function setSitename($sitename);
-
-    /**
-     * @param $url
-     */
-    public function setUrl($url);
-
-    /**
-     * @param $title
-     */
-    public function setTitle($title);
-
-    /**
-     * @param $description
-     */
-    public function setDescription($description);
-
-    /**
-     * @param $image
-     */
-    public function setImage($image);
 }
--- Model/SocialMarkup/AbstractSocialMarkup.php
+++ Model/SocialMarkup/AbstractSocialMarkup.php
@@ -12,17 +12,22 @@ namespace Paskel\Seo\Model\SocialMarkup;
 use Magento\CatalogGraphQl\Model\Resolver\Products\DataProvider\Image\Placeholder as PlaceholderProvider;
 use Magento\Framework\Exception\LocalizedException;
 use Magento\StoreGraphQl\Model\Resolver\Store\StoreConfigDataProvider;
+use Paskel\Seo\Api\Data\OpenGraphInterface;
 use Paskel\Seo\Api\Data\SocialMarkupInterface;
 use Paskel\Seo\Helper\Hreflang as HreflangHelper;
 use Paskel\Seo\Helper\SocialMarkup as SocialMarkupHelper;
 
 /**
  * Class AbstractSocialMarkup
- * @package Paskel\Seo\Model\Resolver\SocialMarkup
+ * @package Paskel\Seo\Model\SocialMarkup
  */
 abstract class AbstractSocialMarkup implements SocialMarkupInterface
 {
-    protected $socialMarkups = [];
+
+    /**
+     * @var array
+     */
+    protected $tags = [];
 
     /**
      * @var StoreConfigDataProvider
@@ -43,7 +48,6 @@ abstract class AbstractSocialMarkup implements SocialMarkupInterface
      * @var SocialMarkupHelper
      */
     protected SocialMarkupHelper $socialMarkupHelper;
-
     /**
      * AbstractSocialMarkup constructor.
      *
@@ -62,107 +66,5 @@ abstract class AbstractSocialMarkup implements SocialMarkupInterface
         $this->placeholderProvider = $placeholderProvider;
         $this->hreflangHelper = $hreflangHelper;
         $this->socialMarkupHelper = $socialMarkupHelper;
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function setType($type) {
-        $this->wrapSocialMarkup(self::TYPE, $type);
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function setLocale($locale) {
-        $this->wrapSocialMarkup(self::LOCALE, $locale);
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function setSitename($sitename) {
-        $this->wrapSocialMarkup(self::SITENAME, $sitename);
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function setUrl($url) {
-        $this->wrapSocialMarkup(self::URL, $url);
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function setTitle($title) {
-        $this->wrapSocialMarkup(self::TITLE, $title);
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function setDescription($description) {
-        $this->wrapSocialMarkup(self::DESCRIPTION, $description);
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function setImage($image) {
-        $this->wrapSocialMarkup(self::IMAGE, $image);
-    }
-
-    /**
-     * Returns the specific property-content properly wrapped
-     *
-     * @param $property
-     * @param $content
-     */
-    public function wrapSocialMarkup($property, $content) {
-        if (!($this->socialMarkupHelper->hideUnsetPropertiesInGraphQl() and $content == null)) {
-            array_push($this->socialMarkups,
-                [
-                    self::PROPERTY => $property,
-                    self::CONTENT => $content
-                ]
-            );
-        }
-    }
-
-    /**
-     * Retrieve sitename from config options
-     *
-     * @param int|null $storeId
-     */
-    public function setSitenameByStore($storeId = null) {
-        $sitename = $this->socialMarkupHelper->getSitename($storeId);
-        if ($sitename) {
-            $this->setSitename($this->socialMarkupHelper->getSitename($storeId));
-        } else {
-            $this->setSitename(self::SITENAME_VALUE);
-        }
-    }
-
-    /**
-     * Retrieve full url using hreflang table.
-     *
-     * @param $entityId
-     * @param $entityType
-     * @param $storeId
-     * @return string|null
-     */
-    public function retrieveUrl($entityId, $entityType, $storeId) {
-        try {
-            $hreflang = $this->hreflangHelper->getStoreHreflang(
-                $entityId,
-                $entityType,
-                $storeId
-            );
-            return $hreflang->getUrl();
-        }
-        catch (LocalizedException $e) {
-            return null;
-        }
     }
 }
